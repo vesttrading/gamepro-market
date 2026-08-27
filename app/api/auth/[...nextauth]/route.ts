dfaa50729b6114464bc8a39cfa5b0861f91419b9
GamePro <vesttrading.pro@gmail.com>
2026-08-27 19:53:53 +0300

    Update BattleNetProvider issuer URL for OAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ const handler = NextAuth({
     BattleNetProvider({
       clientId: process.env.BATTLE_NET_CLIENT_ID!,
       clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
-      issuer: "https://battle.net",
+      issuer: "https://battle.net"/oauth
       authorization: {
         params: {
           scope: "openid wow.profile",
