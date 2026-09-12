76759289d2f477fc264afe20172410767c46327f
GamePro <vesttrading.pro@gmail.com>
2026-09-12 09:07:59 +0300

    Update BattleNetProvider issuer URL

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ const handler = NextAuth({
     BattleNetProvider({
       clientId: process.env.BATTLE_NET_CLIENT_ID!,
       clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
-     issuer: "https://eu.battle.net/oauth",
+     issuer: "https://eu.battle.net",
     }),
   ],
 
