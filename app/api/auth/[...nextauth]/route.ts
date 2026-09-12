5f18cb9f14f0d5b05bff943cc68eaaca07a186d9
GamePro <vesttrading.pro@gmail.com>
2026-09-12 14:32:48 +0300

    Refactor NextAuth configuration and export options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,45 +2,17 @@
 import NextAuth from "next-auth";
 import BattleNetProvider from "next-auth/providers/battlenet";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     BattleNetProvider({
       clientId: process.env.BATTLE_NET_CLIENT_ID!,
       clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
       issuer: "https://eu.battle.net/oauth",
-      checks: ["state"],
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.battletag,
-          email: null, 
-          image: null,
-        };
-      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  
-  cookies: {
-    callbackUrl: {
-      // 👇 Обернули в кавычки "" — теперь TypeScript не будет ругаться
-      name: "__Secure-next-auth.callback-url",
-      options: {
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
-    state: {
-      // 👇 Обернули в кавычки ""
-      name: "__Secure-next-auth.state",
-      options: {
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
-  },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
