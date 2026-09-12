4a84c89c9a82188afd154680cc6f36f6699984a1
GamePro <vesttrading.pro@gmail.com>
2026-09-12 16:32:58 +0300

    Use non-null assertion for BattleNet credentials

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import BattleNetProvider from "next-auth/providers/battlenet";
 const handler = NextAuth({
   providers: [
     BattleNetProvider({
-      clientId: process.env.BATTLE_NET_CLIENT_ID,
-      clientSecret: process.env.BATTLE_NET_CLIENT_SECRET,
+      clientId: process.env.BATTLE_NET_CLIENT_ID!,
+      clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
       issuer: "https://eu.battle.net/oauth",
     }),
   ],
