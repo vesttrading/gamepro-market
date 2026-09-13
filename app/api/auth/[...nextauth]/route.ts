2e73d7c7fe84ceb59bae0a36a947a1104e485cf3
GamePro <vesttrading.pro@gmail.com>
2026-09-13 14:52:11 +0300

    Refactor NextAuth initialization in route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import BattleNetProvider from "next-auth/providers/battlenet";
 
-const authOptions = {
+const handler = NextAuth({
   providers: [
     BattleNetProvider({
       clientId: process.env.BATTLE_NET_CLIENT_ID!,
@@ -12,8 +12,6 @@ const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   debug: true,
-};
-
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
