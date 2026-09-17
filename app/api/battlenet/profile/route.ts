c9863e44bdd6b21322f3cc17cfd416e690d90011
GamePro <vesttrading.pro@gmail.com>
2026-09-17 13:15:45 +0300

    Fix import path for authOptions in route.ts

--- app/api/battlenet/profile/route.ts
+++ app/api/battlenet/profile/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../../lib/auth";
+import { authOptions } from "../../../lib/auth";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
