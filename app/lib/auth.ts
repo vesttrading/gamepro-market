5e2f6acf5a95f3c0349f3c6c4b29a12a5a876797
GamePro <vesttrading.pro@gmail.com>
2026-09-17 11:55:41 +0300

    Add authentication options using BattleNet provider

---
+++ app/lib/auth.ts
@@ -0,0 +1,29 @@
+import { AuthOptions } from "next-auth";
+import BattleNetProvider from "next-auth/providers/battlenet";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    BattleNetProvider({
+      clientId: process.env.BATTLE_NET_CLIENT_ID!,
+      clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
+      issuer: "https://battle.net",
+      checks: ["state", "pkce", "nonce"],
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+};
