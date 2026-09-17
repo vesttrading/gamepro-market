dc7c665c73ce674f9e72590bc532afa118c7ba72
GamePro <vesttrading.pro@gmail.com>
2026-09-17 15:42:48 +0300

    Update BattleNet provider configuration in auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,13 +1,12 @@
-
 import { AuthOptions } from "next-auth";
 import BattleNetProvider from "next-auth/providers/battlenet";
 
 export const authOptions: AuthOptions = {
   providers: [
     BattleNetProvider({
-      clientId: process.env.BATTLE_NET_CLIENT_ID!,
-      clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
-      issuer: "https://eu.battle.net/oauth",
+      clientId: process.env.BATTLENET_CLIENT_ID!,
+      clientSecret: process.env.BATTLENET_CLIENT_SECRET!,
+      issuer: "https://battle.net",
       checks: ["state", "pkce", "nonce"],
     }),
   ],
@@ -18,13 +17,9 @@ export const authOptions: AuthOptions = {
       }
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }: any) {
       session.accessToken = token.accessToken;
       return session;
     },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "jwt",
   },
 };
