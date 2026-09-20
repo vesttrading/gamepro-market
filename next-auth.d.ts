30bae9800de3fb4c9a4c2a78382e38baa0c83e96
GamePro <vesttrading.pro@gmail.com>
2026-09-20 13:10:21 +0300

    Add TypeScript definitions for NextAuth session and JWT

---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth, { DefaultSession } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session {
+    error?: string;
+    user: DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    error?: string;
+    accessToken?: string;
+    expiresAt?: number;
+  }
+}
