36aec0d723d3e8919fe364ae48d5ad0af471236c
GamePro <vesttrading.pro@gmail.com>
2026-09-23 09:19:08 +0300

    Enhance BattleNetProvider with login prompt
    Added authorization prompt parameter to BattleNetProvider configuration.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,17 @@ import BattleNetProvider from "next-auth/providers/battlenet";
 
 const handler = NextAuth({
   providers: [
-    BattleNetProvider({
-      clientId: process.env.BATTLE_NET_CLIENT_ID!,
-      clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
-      issuer: "https://eu.battle.net/oauth",
-      checks: ["state", "pkce", "nonce"],
-    }),
-  ],
+  BattleNetProvider({
+  clientId: process.env.BATTLE_NET_CLIENT_ID!,
+  clientSecret: process.env.BATTLE_NET_CLIENT_SECRET!,
+  issuer: "https://eu.battle.net/oauth",
+  checks: ["state", "pkce", "nonce"],
+  authorization: {
+    params: {
+      prompt: "login",
+    },
+  },
+}),
 
   secret: process.env.NEXTAUTH_SECRET,
 
