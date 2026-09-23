0639b4eeede1408580882a5537326859658ad62e
GamePro <vesttrading.pro@gmail.com>
2026-09-23 08:07:58 +0300

    Remove register link from Header component
    Removed the register link and its associated click handler from the Header component.

--- components/Header.tsx
+++ components/Header.tsx
@@ -68,13 +68,6 @@ export default function Header({ lang, setLang, t }: HeaderProps) {
           </a>
           <a href="#reviews" style={{ color: "#bdc4da" }}>
             {t.reviews}
-          </a>
-         <a href="#" onClick={(e) => { e.preventDefault();
-            window.location.href = "/api/auth/signin/battlenet";
-          }}
-           style={{ color: "#bdc4da" }}>
-           {t.register}
-         </a>
           <a href="guilds" style={{ color: "#bdc4da" }}>
             {t.guilds}
           </a>
