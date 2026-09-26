95481b64536e28b279b2e68695f491d56097028d
GamePro <vesttrading.pro@gmail.com>
2026-09-26 11:14:33 +0300

    Refactor return statement for player page

--- app/player/[id]/page.tsx
+++ app/player/[id]/page.tsx
@@ -27,9 +27,8 @@ export default async function PlayerProfilePage({
 
   const verified = player.source_verified === true;
  
- return (
-    
-   <div
+  return (
+     <div
       style={{
         minHeight: "100vh",
         background:
